b14dd44ffe2aefe9ed48877d5345b97565c40ff2
mutistic <mutistic@qq.com>
2018-07-25 15:21:58 +0800

    v1.2.7 测试redis

--- com.mutistic.boot/README.md
+++ com.mutistic.boot/README.md
@@ -24,8 +24,9 @@ Spring Boot 是伴随着[Spring4.0](https://github.com/mutistic/mutistic.spring/
 12. <a href="#a_event">简单实现自定义事件和监听</a>
 13. <a href="#a_initializers">Spring初始化处理器</a>
 14. <a href="#a_runner">Spring容器运行后的最后一次回调接口</a>
-97. <a href="#a_pit">spring boot 入坑总结</a>
-98. <a href="#a_sql">sql</a>
+96. <a href="#a_pit">spring boot 入坑总结</a>
+97. <a href="#a_sql">sql</a>
+98. <a href="#a_sql">ea</a>
 99. <a href="#a_down">down</a>
 
 ---
@@ -2287,8 +2288,12 @@ public class MainByRunner {
 [pit4、继承JpaRepository时报错[JPA]](https://github.com/mutistic/mutistic.spring/blob/master/com.mutistic.boot/notes/pit/pit4_%E7%BB%A7%E6%89%BFJpaRepository%E6%97%B6%E6%8A%A5%E9%94%99%5BJPA%5D.docx)<br/>
 
 ---
-## <a id="a_sql">II、[sql](https://github.com/mutistic/mutistic.spring/tree/master/com.mutistic.boot/notes/sql)</a> <a href="#a_pit">last</a> <a href="#a_down">Down</a>
+## <a id="a_sql">II、[sql](https://github.com/mutistic/mutistic.spring/tree/master/com.mutistic.boot/notes/sql)</a> <a href="#a_pit">last</a> <a href="#a_ea">Down</a>
 [ddl_creater_database_study](https://github.com/mutistic/mutistic.spring/blob/master/com.mutistic.boot/notes/sql/ddl_creater_database_study.sql)
+
+---
+## <a id="a_ea">III、[EA](https://github.com/mutistic/mutistic.spring/tree/master/com.mutistic.boot/notes/ea)</a> <a href="#a_sql">last</a> <a href="#a_down">Down</a>
+[E1_SpringApplication运行分析.eap](https://github.com/mutistic/mutistic.spring/blob/master/com.mutistic.boot/notes/ea/E1_SpringApplication\350\277\220\350\241\214\345\210\206\346\236\220.eap)
 
 
 ---
---
+++ com.mutistic.boot/src/main/java/com/mutistic/start/redis/RedisConfiguration.java
@@ -0,0 +1,197 @@
+package com.mutistic.start.redis;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.*;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import com.alibaba.fastjson.support.spring.GenericFastJsonRedisSerializer;
+
+import net.bytebuddy.utility.JavaType;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+@Configuration
+@EnableCaching
+public class RedisConfiguration extends CachingConfigurerSupport{
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private int port;
+
+    @Value("${spring.redis.password}")
+    private String password;
+
+    @Value("${spring.redis.timeout}")
+    private int timeout;
+
+    @Value("${spring.redis.jedis.pool.max-idle}")
+    private int maxIdle;
+
+    @Value("${spring.redis.jedis.pool.max-wait}")
+    private long maxWaitMillis;
+
+    @Value("${shiro.session.session-prefix}")
+    private String sessionPrefix;
+
+    @Value("${shiro.session.session-time}")
+    private int sessionTime;
+
+    @Value("${shiro.cache.cache-prefix}")
+    private String cachePrefix;
+
+    @Value("${shiro.cache.cache-time}")
+    private int cacheTime;
+
+    @Bean
+    public JedisPool redisPoolFactory() {
+        logger.info("JedisPool注入成功！！");
+        JedisPool jedisPool = new JedisPool(getJedisPoolConfig(), host, port, timeout, password);
+        // JedisPool jedisPool = new JedisPool(getJedisPoolConfig(), host, port, timeout);
+        return jedisPool;
+    }
+
+    @Bean
+    public JedisPoolConfig getJedisPoolConfig(){
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        jedisPoolConfig.setMaxIdle(maxIdle);
+        jedisPoolConfig.setMaxWaitMillis(maxWaitMillis);
+        return jedisPoolConfig;
+    }
+
+    @Bean
+    public JedisConnectionFactory jedisConnectionFactory(){
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(host);
+        redisStandaloneConfiguration.setPassword(RedisPassword.of(password));
+        redisStandaloneConfiguration.setPort(port);
+        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory(redisStandaloneConfiguration);
+        logger.info("JedisConnectionFactory注入成功！！");
+        return jedisConnectionFactory;
+    }
+
+    @Bean
+    public CacheManager cacheManager(JedisConnectionFactory jedisConnectionFactory) {
+        logger.info("cacheManager注入成功！！");
+
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig();
+
+        RedisCacheWriter redisCacheWriter = RedisCacheWriter.nonLockingRedisCacheWriter(jedisConnectionFactory);
+        RedisCacheManager redisCacheManager = new RedisCacheManager(redisCacheWriter, redisCacheConfiguration);
+        //设置缓存过期时间
+
+        return redisCacheManager;
+    }
+
+    @Bean(name = "redisTemplate")
+    public RedisTemplate<String, Object> getRedisTemplate() {
+        logger.info("redisTemplatet注入成功！！");
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new JdkSerializationRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new GenericFastJsonRedisSerializer());
+        redisTemplate.setConnectionFactory(jedisConnectionFactory());
+        return redisTemplate;
+    }
+
+    public String getSessionPrefix() {
+        return sessionPrefix;
+    }
+
+    public void setSessionPrefix(String sessionPrefix) {
+        this.sessionPrefix = sessionPrefix;
+    }
+
+    public String getCachePrefix() {
+        return cachePrefix;
+    }
+
+    public void setCachePrefix(String cachePrefix) {
+        this.cachePrefix = cachePrefix;
+    }
+
+    public int getSessionTime() {
+        return sessionTime;
+    }
+
+    public void setSessionTime(int sessionTime) {
+        this.sessionTime = sessionTime;
+    }
+
+    public int getCacheTime() {
+        return cacheTime;
+    }
+
+    public void setCacheTime(int cacheTime) {
+        this.cacheTime = cacheTime;
+    }
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getTimeout() {
+        return timeout;
+    }
+
+    public void setTimeout(int timeout) {
+        this.timeout = timeout;
+    }
+
+    public int getMaxIdle() {
+        return maxIdle;
+    }
+
+    public void setMaxIdle(int maxIdle) {
+        this.maxIdle = maxIdle;
+    }
+
+    public long getMaxWaitMillis() {
+        return maxWaitMillis;
+    }
+
+    public void setMaxWaitMillis(long maxWaitMillis) {
+        this.maxWaitMillis = maxWaitMillis;
+    }
+
+}
